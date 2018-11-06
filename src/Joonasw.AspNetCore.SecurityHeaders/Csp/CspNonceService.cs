bf39bb51af4c5eaf834a164fa63cd39d2d4d1250
Rob Hux <rhux@array-advisors.com>
2018-11-06 17:07:52 -0500

    adding in changes for the nonce service, that seemed for us to cause bugs. When we changed the nonce to URL friendly and specified the tag attribute it was getting included

--- src/Joonasw.AspNetCore.SecurityHeaders/Csp/CspNonceService.cs
+++ src/Joonasw.AspNetCore.SecurityHeaders/Csp/CspNonceService.cs
@@ -6,6 +6,12 @@ namespace Joonasw.AspNetCore.SecurityHeaders.Csp
     public class CspNonceService : ICspNonceService
     {
         private readonly string _nonce;
+        private static char base64PadCharacter = '=';
+        private static string doubleBase64PadCharacter = "==";
+        private static char base64Character62 = '+';
+        private static char base64Character63 = '/';
+        private static char base64UrlCharacter62 = '-';
+        private static char _base64UrlCharacter63 = '_';
 
         public CspNonceService(int nonceByteAmount = 32)
         {
@@ -15,7 +21,15 @@ namespace Joonasw.AspNetCore.SecurityHeaders.Csp
                 rng.GetBytes(nonceBytes);
             }
 
-            _nonce = Convert.ToBase64String(nonceBytes);
+          var s = Convert.ToBase64String(nonceBytes);
+          // a base64String can have elements that aren't URL friendly. In testing chrome appeared to ignore nonces that weren't URL friendly (or the tag helper did).
+          // the replacement code comes from Microsoft.IdentityModel.Tokens.Base64UrlEncoder
+          s = s.Split(base64PadCharacter)[0]; // Remove any trailing padding
+          s = s.Replace(base64Character62, base64UrlCharacter62);  // 62nd char of encoding
+          s = s.Replace(base64Character63, _base64UrlCharacter63);  // 63rd char of encoding
+
+          _nonce = s;
+            
         }
 
         public string GetNonce()
