dca41c3230620a3166f3e78cc9bb6cf718b8d71b
Rob Hux <rhux@array-advisors.com>
2018-12-03 13:56:43 -0500

    adding naming changes, per request

--- src/Joonasw.AspNetCore.SecurityHeaders/Csp/CspNonceService.cs
+++ src/Joonasw.AspNetCore.SecurityHeaders/Csp/CspNonceService.cs
@@ -6,12 +6,12 @@ namespace Joonasw.AspNetCore.SecurityHeaders.Csp
     public class CspNonceService : ICspNonceService
     {
         private readonly string _nonce;
-        private static char base64PadCharacter = '=';
-        private static string doubleBase64PadCharacter = "==";
-        private static char base64Character62 = '+';
-        private static char base64Character63 = '/';
-        private static char base64UrlCharacter62 = '-';
-        private static char _base64UrlCharacter63 = '_';
+        private const char Base64PadCharacter = '=';
+        private const string DoubleBase64PadCharacter = "==";
+        private const char Base64Character62 = '+';
+        private const char Base64Character63 = '/';
+        private const char Base64UrlCharacter62 = '-';
+        private const char Base64UrlCharacter63 = '_';
 
         public CspNonceService(int nonceByteAmount = 32)
         {
@@ -24,9 +24,9 @@ namespace Joonasw.AspNetCore.SecurityHeaders.Csp
           var s = Convert.ToBase64String(nonceBytes);
           // a base64String can have elements that aren't URL friendly. In testing chrome appeared to ignore nonces that weren't URL friendly (or the tag helper did).
           // the replacement code comes from Microsoft.IdentityModel.Tokens.Base64UrlEncoder
-          s = s.Split(base64PadCharacter)[0]; // Remove any trailing padding
-          s = s.Replace(base64Character62, base64UrlCharacter62);  // 62nd char of encoding
-          s = s.Replace(base64Character63, _base64UrlCharacter63);  // 63rd char of encoding
+          s = s.Split(Base64PadCharacter)[0]; // Remove any trailing padding
+          s = s.Replace(Base64Character62, Base64UrlCharacter62);  // 62nd char of encoding
+          s = s.Replace(Base64Character63, Base64UrlCharacter63);  // 63rd char of encoding
 
           _nonce = s;
             
